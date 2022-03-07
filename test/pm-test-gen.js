96f658825ce5b3e39dc8a1dc23d16d80d46deea0
Dan Heslinga <dan@heslinga.us>
2022-03-07 13:59:58 -1000

    do i have the pm-test-gen.js right yet?

--- test/pm-test-gen.js
+++ test/pm-test-gen.js
@@ -4,21 +4,70 @@ function expectOK(testCase) {
 
 function expectCards(testCase) {
   if (testCase.expected != null) {
-    return testCase.expected.Recommendation != null;
+    return (
+      testCase.expected.YesRecommendation != null ||
+      testCase.expected.NeedMoreInformation != null
+    );
   }
 }
 
 function expectCardsContent(testCase) {
   if (expectCards(testCase)) {
-    return {
-      summary: 'Statin Use for the Primary Prevention of CVD',
-      indicator: 'info',
-      detail: testCase.expected.Recommendation,
-      source: {
-        label: 'CDS Connect: Statin Use for the Primary Prevention of CVD in Adults',
-        url: 'https://cds.ahrq.gov/cdsconnect/artifact/statin-use-primary-prevention-cvd-adults'
-      }
-    };
+    const cardArray = [];
+    if (testCase.expected.YesRecommendation != null) {
+      cardArray.push({
+        uuid: 4,
+        summary: "Statin Use for the Primary Prevention of CVD",
+        indicator: "info",
+        detail: testCase.expected.YesRecommendation,
+        source: {
+          label:
+            "CDS Connect: Statin Use for the Primary Prevention of CVD in Adults",
+          url: "https://cds.ahrq.gov/cdsconnect/artifact/statin-use-primary-prevention-cvd-adults",
+        },
+        overrideReasons: [
+          {
+            uuid: "4",
+            display: "Done reviewing",
+          },
+        ],
+        extension: {
+          grade: "${RecommendationGrade}",
+          rationale: "${Rationale}",
+        },
+      });
+    }
+    if (testCase.expected.NeedMoreInformation != null) {
+      cardArray.push({
+        summary: "More information needed to complete Statin Use assessment",
+        indicator: "warning",
+        detail: testCase.expected.NeedMoreInformation,
+        source: {
+          label:
+            "CDS Connect: Statin Use for the Primary Prevention of CVD in Adults",
+          url: "https://cds.ahrq.gov/cdsconnect/artifact/statin-use-primary-prevention-cvd-adults",
+        },
+      });
+    }
+    return cardArray;
+    // return [{
+    //   uuid: 4,
+    //   summary: 'Statin Use for the Primary Prevention of CVD',
+    //   indicator: 'info',
+    //   detail: testCase.expected.YesRecommendation,
+    //   source: {
+    //     label: 'CDS Connect: Statin Use for the Primary Prevention of CVD in Adults',
+    //     url: 'https://cds.ahrq.gov/cdsconnect/artifact/statin-use-primary-prevention-cvd-adults'
+    //   }
+    // },{
+    //   summary: 'More information needed to complete Statin Use assessment',
+    //   indicator: 'warning',
+    //   detail: testCase.expected.NeedMoreInformation,
+    //   source: {
+    //     label: 'CDS Connect: Statin Use for the Primary Prevention of CVD in Adults',
+    //     url: 'https://cds.ahrq.gov/cdsconnect/artifact/statin-use-primary-prevention-cvd-adults'
+    //   }
+    // }];
   }
 }
 
